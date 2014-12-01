8b0b84b3764ad3e7e4fec6eeace60ced2c41cb53
Nic Mitchell <nic@nicmitchell.com>
2014-12-01 16:09:25 -0700

    added Kaltura credentials

--- server/kalturaConfig.php
+++ server/kalturaConfig.php
@@ -3,14 +3,14 @@
 // Get a free trial at: http://corp.kaltura.com
 // In your Kaltura account, get the partner Id and API Admin Secret from:
 // http://www.kaltura.com/index.php/kmc/kmc4#account|integration
-define("PARTNER_ID", 000);
-define("ADMIN_SECRET",'xxx');
-define("USER_SECRET", 'xxx');
-define("PLAYER_UICONF_ID", 8907861);
-define("BUY_BUTTON_PLAYER_UICONF_ID", 8907871);
-define("PAYPAL_METADATA_PROFILE_ID", 27611);
-define("PAYPAL_CATEGORY_METADATA_PROFILE_ID", 27621);
-define("PAYPAL_USER_METADATA_PROFILE_ID", 27651);
+define("PARTNER_ID", '1357241');
+define("ADMIN_SECRET",'1179e62370a3731977bf9a4c6dba3590');
+define("USER_SECRET", '7501bdbfadc96b24828ce542b5c66b7a');
+define("PLAYER_UICONF_ID", 27216752);
+define("BUY_BUTTON_PLAYER_UICONF_ID", null);
+define("PAYPAL_METADATA_PROFILE_ID", 4376762);
+define("PAYPAL_CATEGORY_METADATA_PROFILE_ID", 4376772);
+define("PAYPAL_USER_METADATA_PROFILE_ID", 4376782);
 //Generates a USER ID based on the machine name and IP address.
 function getRealIpAddr() {
 	if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
